06da4f8dc54daaf123d22156c6841bacfa90876f
Christopher O'Driscoll <77005138+krilleellerstoffe@users.noreply.github.com>
2024-03-07 10:43:31 +0100

    update post method

--- backend/mono/model/user.js
+++ backend/mono/model/user.js
@@ -1,13 +1,38 @@
 const axios = require('axios')
 const user_endpoint = 'http://localhost:1337/api/users'
 
-async function createUser() {
+// async function createUser() {
 
-    let user_data = {
-        username: 'Bob'
-    }  
-    const response = axios.post(user_endpoint, user_data)
-    console.log(response)
-}
+//     let data = {
+//         username: 'test',  
+//         email: 'test@test.com',
+//         password: 'Password'
+//     }  
+//     const response = axios.post(user_endpoint, data)
+//     console.log(response)
+// }
 
-createUser()
+// createUser()
+
+axios.post('http://localhost:1337/api/users', {
+    username: 'test',  
+    email: 'test@test.com',
+    password: 'Password',
+    role: 'Authenticated'
+  })
+  .then(response => {
+    console.log('User profile', response.data.user);
+    console.log('User token', response.data.jwt);
+  })
+  .catch(error => {
+    console.log('An error occurred:', error.response);
+  });
+
+//   axios.get('http://localhost:1337/api/items', {
+//     headers: {
+//         Accept: '*/*'
+//     }
+//   })
+//   .then(response => {
+//     console.log(response.data)
+//   })
